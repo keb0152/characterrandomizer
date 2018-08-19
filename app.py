c6841aa69330ce7be4a3384713b25056f22ca521
KFarmer <katebfarmer@gmail.com>
2018-08-18 20:09:22 -0400

    Default character generation working as expected. Next step: allow user to enter in their own values

--- app.py
+++ app.py
@@ -1,23 +1,46 @@
-from flask import Flask
+from flask import Flask, request
 from flask import render_template
 from flask_bootstrap import Bootstrap
+from theapp import models
 
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+CharacterMaker = models.characterMaker()
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/defaults')
+
+@app.route('/defaults', methods=['GET','POST'])
 def defaultValues():
-    #TODO: Decide if blank form or Response
+    if request.method == 'POST':
+        values = {'sex': None,
+                  'age': None,
+                  'gender': None,
+                  'race': None}
+        doSex = request.values.get('doSex')
+        doAge = request.values.get('doAge')
+        doGender = request.values.get('doGender')
+        doRace = request.values.get('doRace')
+        if doSex:
+            values['sex'] = CharacterMaker.sexuality()
+        if doAge:
+            values['age'] = CharacterMaker.age()
+        if doGender:
+            values['gender'] = CharacterMaker.gender()
+        if doRace:
+            values['race'] = CharacterMaker.race()
+        return render_template('useDefaults.html', age = values['age'], sex = values['sex'],
+                                                   gender = values['gender'], race = values['race'])
+    else:
+        return render_template('useDefaults.html')
 
-    #TODO: Generate Response
-
-    #TODO: Update return value
-    return render_template('index.html')
+@app.route('/notDefaults', methods=['GET','POST'])
+def noDefaultValues():
+    return render_template('dontUseDefaults.html')
 
 
 if __name__ == '__main__':
--- theapp/models.py
+++ theapp/models.py
@@ -7,7 +7,7 @@ sexualityList = ['straight','gay','bisexual']
 
 class characterMaker():
     def race(self, list=raceList):
-        selection = random.randint(0,len(list))
+        selection = random.randint(0,len(list)-1)
         return list[selection]
 
     def age(self, low_age=12, high_age=100):
@@ -15,9 +15,9 @@ class characterMaker():
         return selection
 
     def gender(self, list=genderList):
-        selection = random.randint(0, len(list))
+        selection = random.randint(0, len(list)-1)
         return list[selection]
 
     def sexuality(self, list=sexualityList):
-        selection = random.randint(0, len(list))
+        selection = random.randint(0, len(list)-1)
         return list[selection]
